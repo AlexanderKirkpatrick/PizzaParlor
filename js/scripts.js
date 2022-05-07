3594258ddf77f6ce9bfd70fb1229b90e3e020eba
Alexander <knibz@protonmail.com>
2022-05-06 23:57:02 -0700

    edit pizzaSize prototype, add some UI for testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,56 @@
-function Pizza (crust, sauce, meats, toppings, size) {
+function Pizza (size, crust, sauce, meats, toppings) {
+  this.size = size;
   this.crust = crust;
   this.sauce = sauce;
   this.meats = meats;
   this.toppings = toppings;
-  this.size = size;
   this.cost = 0;
 }
 
-Pizza.prototype.size = function (pizzaSize) {
-  let size = pizzaSize;
+Pizza.prototype.pizzaSize = function (size) {
+  let pizzaSize = size;
   const sizeSmall = "Small";
   const sizeMedium = "Medium";
   const sizeLarge = "Large"
-  for (let s = 0; s <= sizeSmall.length -1; s++) {
-    for (let m = 0; m <= sizeMedium.length -1; m++) {
-      for (let l = 0; l <= sizeLarge.length -1; l++) {
-        if (sizeSmall[s].includes(size)) {
-          this.cost += 5;
-          break;
-        } else if (sizeMedium[m].includes(size)) {
-          this.cost += 10;
-          break;
-        } else if (sizeLarge[l].includes(size)) {
-          this.cost += 15;
-          break;
-        }
-      }
+  if (sizeSmall.includes(pizzaSize)) {
+    this.cost += 5;  
+      } else if (sizeMedium.includes(pizzaSize)) {
+        this.cost += 10;
+      } else if (sizeLarge.includes(pizzaSize)) {
+        this.cost += 15;
     }
   }
-}
+    
+  
 
-Pizza
+
+
+
+
+
+$(document).ready(function() {
+  $("form#pizza-picker").submit(function(event) {
+    event.preventDefault();
+
+    const sizeOptions = $("select#sizeOptions").val();
+    const crustOptions = $("select#crustOptions").val();
+    const sauceOptions = $("select#sauceOptions").val();
+    const meatOptions = $("select#meatOptions").val(); 
+    const toppingsOptions = $("select#toppingsOptions").val();
+
+    let newPizza = new Pizza(sizeOptions, crustOptions, sauceOptions, meatOptions, toppingsOptions);
+    newPizza.pizzaSize(sizeOptions);
+    //newPizza.pizzaCrust(crustOptions);
+    //newPizza.pizzaSauce(sauceOptions);
+    //newPizza.pizzaMeat(meatOptions);
+    //newPizza.pizzaToppings(toppingsOptions);
+
+    const totalPrice = newPizza.cost;
+    $("#price").show();
+    $(".total").text(totalPrice);
+    $("#pizza-picker").each(function(){
+      this.reset();
+    });
+  });
+});
 
