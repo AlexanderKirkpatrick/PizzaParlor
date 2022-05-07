b4f7cfd8142b2cdcab447500f1632ba6b2f25ac6
Alexander <knibz@protonmail.com>
2022-05-06 23:01:45 -0700

    add passing pizzaSize prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
-function Pizza (crust, sauce, toppings, size) {
+function Pizza (crust, sauce, meats, toppings, size) {
   this.crust = crust;
   this.sauce = sauce;
+  this.meats = meats;
   this.toppings = toppings;
   this.size = size;
   this.cost = 0;
 }
+
+Pizza.prototype.size = function (pizzaSize) {
+  let size = pizzaSize;
+  const sizeSmall = "Small";
+  const sizeMedium = "Medium";
+  const sizeLarge = "Large"
+  for (let s = 0; s <= sizeSmall.length -1; s++) {
+    for (let m = 0; m <= sizeMedium.length -1; m++) {
+      for (let l = 0; l <= sizeLarge.length -1; l++) {
+        if (sizeSmall[s].includes(size)) {
+          this.cost += 5;
+          break;
+        } else if (sizeMedium[m].includes(size)) {
+          this.cost += 10;
+          break;
+        } else if (sizeLarge[l].includes(size)) {
+          this.cost += 15;
+          break;
+        }
+      }
+    }
+  }
+}
+
+Pizza
 
